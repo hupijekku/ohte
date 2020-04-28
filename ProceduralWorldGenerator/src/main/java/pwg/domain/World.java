929ee0bbd7ad671a5af1b0115dbc301243e26a71
Eemeli Saarinen <saarineneemelia@gmail.com>
2020-04-28 22:25:41 +0300

    Javadocs and comments

--- ProceduralWorldGenerator/src/main/java/pwg/domain/World.java
+++ ProceduralWorldGenerator/src/main/java/pwg/domain/World.java
@@ -1,7 +1,7 @@
 package pwg.domain;
 
 /**
- *
+ * Contains the data and structure of a world to be generated.
  * @author Hupijekku
  */
 public class World {
@@ -12,28 +12,53 @@ public class World {
     private int mountainousness = 0;
     private int vegetation = 0;
     
+    
+    /**
+     * Empty constructor for a World-object. 
+     * Sets the default world type "World" with size 0
+     * and an empty tile-map
+     */
     public World() {
         this.type = WorldType.WORLD;
         this.size = 0;
         this.tilemap = new Tile[0][0];
     }
     
+    /**
+     * Constructor that creates a World-object with given type and size.
+     * @param type  Type of the world (Enum WorldType)
+     * @param size  Size of the world (Length of a side of square)
+     */
     public World(WorldType type, int size) {
         this.type = type;
         this.size = size;
         this.tilemap = new Tile[size][size];
     }
-    
+    /**
+     * Constructor that creates a World-object with a pre-initialized tile-map
+     * @param type  Type of the world (Enum WorldType)
+     * @param size  Size of the world (Length of a side of square)
+     * @param tilemap   2 dimensional square array of Tile-objects
+     */
     public World(WorldType type, int size, Tile[][] tilemap) {
         this.type = type;
         this.size = size;
         this.tilemap = tilemap;
     }
-    
+    /**
+     * Returns the 2 dimensional tile-map array of this world.
+     * @return 2 dimensional square array of tiles.
+     */
     public Tile[][] getTiles() {
         return this.tilemap;
     }
     
+    /**
+     * Returns the Tile-object with the given coordinates from the World's tile-map.
+     * @param x The x-coordinate of the tile.
+     * @param y The y-coordinate of the tile.
+     * @return Tile from the given coordinates.
+     */
     public Tile getTileAt(int x, int y) {
         if (x < this.size && y < this.size && x > 0 && y > 0) {
             return this.tilemap[x][y];
@@ -41,35 +66,62 @@ public class World {
             return null;
         }
     }
-    
+    /**
+     * Returns the size of the world. Side length of the square shaped world.
+     * @return World size
+     */
     public int getSize() {
         return this.size;
     }
-    
+    /**
+     * Returns the type of the world. WorldType enum.
+     * @return WorldType enum
+     */
     public WorldType getType() {
         return this.type;
     }
-    
+    /**
+     * Setter for the private field humidity. Changes the amount of water
+     * present in a World when generated.
+     * @param hum Humidity of the world.
+     */
     public void setHumidity(int hum) {
         this.humidity = hum;
     }
-    
+    /**
+     * Setter for the private field mountainousness. Changes the average height
+     * of the world and the amount of large mountains.
+     * @param mount Average height and mountainousness of the world.
+     */
     public void setMountainousness(int mount) {
         this.mountainousness = mount;
     }
-    
+    /**
+     * Setter for the private field vegetation. Changes the amount of plants and 
+     * flora in a world.
+     * @param veg Vegetation and flora of the world.
+     */
     public void setVegetation(int veg) {
         this.vegetation = veg;
     }
-    
+    /**
+     * Getter for the private field humidity
+     * @return The current world humidity.
+     */
     public int getHumidity() {
         return this.humidity;
     }
-    
+    /**
+     * Getter for the private field mountainousness
+     * @return The height and mountainousness modifier.
+     */
     public int getMountainousness() {
         return this.mountainousness;
     }
-    
+    /**
+     * Getter for the private field vegetation.
+     * @return The world vegetation modifier.
+     */
     public int getVegetation() {
         return this.vegetation;
     }
--- ProceduralWorldGenerator/src/main/java/pwg/domain/WorldGenerator.java
+++ ProceduralWorldGenerator/src/main/java/pwg/domain/WorldGenerator.java
@@ -13,7 +13,10 @@ public class WorldGenerator {
     
     private int seed = 0;
     
-    
+    /**
+     * Empty constructor for the WorldGenerator.
+     * Sets a random seed value for this world.
+     */
     public WorldGenerator() {
         Random random = new Random();
         this.seed = random.nextInt();
@@ -30,7 +33,6 @@ public class WorldGenerator {
     * 
     *@return    returns a World object that contains the generated world data.
     */
-  
     public World generate(WorldType type, int size, int humidity, int mountainousness, int vegetation) {
         Tile[][] tilemap = new Tile[size][size];
         for (int x = 0; x < size; x++) {
@@ -49,6 +51,12 @@ public class WorldGenerator {
         return world;
     }
     
+    /**
+     * Creates a 2 dimensional height-map from noise and sets the given World's
+     * tile heights based on that and the given mountainousness modifier.
+     * @param world A world to be generated
+     * @param mount Modifier for the height-map generation
+     */
     private void setMountainousness(World world, int mount) {    
         Tile[][] tilemap = world.getTiles();
         for (int x = 0; x < tilemap.length; x++) {
@@ -62,6 +70,12 @@ public class WorldGenerator {
         }
     }
     
+    /**
+     * Creates a 2 dimensional height-map from noise and sets the given World's
+     * tile humidity based on that and the given humidity modifier.
+     * @param world A world to be generated
+     * @param humidity Modifier for the humidity height-map
+     */
     private void setHumidity(World world, int humidity) {
         Tile[][] tilemap = world.getTiles();
         for (int x = 0; x < tilemap.length; x++) {
@@ -74,7 +88,12 @@ public class WorldGenerator {
             }
         }
     }
-    
+    /**
+     * Creates a 2 dimensional height-map from noise and sets the given World's
+     * tile vegetation based on that and the given vegetation modifier.
+     * @param world A world to be generated
+     * @param vegetation Modifier for the vegetation height-map
+     */
     private void setVegetation(World world, int vegetation) {
         Tile[][] tilemap = world.getTiles();
         for (int x = 0; x < tilemap.length; x++) {
@@ -87,8 +106,22 @@ public class WorldGenerator {
         }
     }
     
+    //TODO: These 3 methods can probably still be merged to one noise function.
+    // They are currently separate as you have to use different seed for each or
+    // the height map for each variable will be the same. 
+    // I had them as one method with a type given as parameter but I separated them
+    // as I wanted to use an entirely different noise algorithm for some of them
+    // Have to decide what to do.
     
     
+    /**
+     * Creates a Perlin noise value for given 2 dimensional coordinates
+     * and scales that based on the world size.
+     * @param x     X coordinate for the noise.
+     * @param y     Y coordinate for the noise.
+     * @param worldSize     Size of the world to be generated.
+     * @return 
+     */
     private double getMountainNoise(int x, int y, int worldSize) {
         float scale = (float) (30 * worldSize) / (1 + Math.abs(worldSize + 20));
         JNoise perlinNoise = JNoise.newBuilder()
@@ -96,6 +129,14 @@ public class WorldGenerator {
         return perlinNoise.getNoise(x / scale, y / scale);
     }
     
+    /**
+     * Creates a Perlin noise value for given 2 dimensional coordinates
+     * and scales that based on the world size.
+     * @param x     X coordinate for the noise.
+     * @param y     Y coordinate for the noise.
+     * @param worldSize     Size of the world to be generated.
+     * @return 
+     */
     private double getHumidityNoise(int x, int y, int worldSize) {
         //float scale = (float) (20-Math.sqrt(worldSize * 0.1));
         float scale = (float) (30 * worldSize) / (1 + Math.abs(worldSize + 20));
@@ -105,6 +146,14 @@ public class WorldGenerator {
         return perlinNoise.getNoise(x / scale, y / scale);
     }
     
+    /**
+     * Creates a Perlin noise value for given 2 dimensional coordinates
+     * and scales that based on the world size.
+     * @param x     X coordinate for the noise.
+     * @param y     Y coordinate for the noise.
+     * @param worldSize     Size of the world to be generated.
+     * @return 
+     */
     private double getVegetationNoise(int x, int y, int worldSize) {
         float scale = (float) Math.sqrt(worldSize);
         JNoise perlinNoise = JNoise.newBuilder()
